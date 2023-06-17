a917f1ffbeb7523a6ef1871524c2a8d1ba684315
Jiger Dhillon <jigersingh29@gmail.com>
2023-06-17 01:21:15 -0400

    DONE

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+       
+            [Theory]
+            [InlineData("racecar", true)]
+            [InlineData("hello", false)]
+            [InlineData("kayak", true)]
+            [InlineData("palindrome", false)]
+            [InlineData("madam", true)]
 
-        }
+            public void PalindromeTest(string word, bool expected)
+            {
+                var test = new WordSmith();
+                var actual = test.IsAPalindrome(word);
+                Assert.Equal(expected, actual);
+            }
     }
 }
